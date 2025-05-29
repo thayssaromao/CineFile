ac1d5883b908d31f4f056038e82b7dde344d4a66
ana23lua <analluy@icloud.com>
2025-05-29 10:06:33 -0300

    eee

---
+++ CineFileApp/homeView.swift
@@ -0,0 +1,18 @@
+//
+//  homeView.swift
+//  CineFileApp
+//
+//  Created by Ana Luisa Luy on 29/05/25.
+//
+
+import SwiftUI
+
+struct homeView: View {
+    var body: some View {
+        Text("home eeeee")
+    }
+}
+
+#Preview {
+    homeView()
+}
