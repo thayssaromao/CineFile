99a6e0826d7eef0e9eb491e64cece6519539481e
DANI <romao@alunos.utfpr.edu.br>
2025-05-30 11:29:24 -0300

    Perfil update

--- CineFileApp/PerfilView.swift
+++ CineFileApp/PerfilView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PerfilView: View {
     var body: some View {
         
-        VStack{
+        VStack(spacing: 32){
             
             ZStack{
                 
@@ -18,43 +18,58 @@ struct PerfilView: View {
                     .resizable()
                     .ignoresSafeArea()
                     
-                Image("tiraPerfil")
+               
                         
                         
-                ZStack {
-                       Circle()
-                           .stroke(Color(red: 29/255, green: 241/255, blue: 145/255), lineWidth: 8)
-                           .frame(width: 150, height: 150)
-
-                       Image("perfil")
-                           .resizable()
-                           .scaledToFill()
-                           .frame(width: 143, height: 143)
-                           .clipShape(Circle())
-                   }
-        
-                VStack(alignment: .center, spacing: 15){
-                    
-                    Text("RATATA")
-                        .font(.largeTitle)
-                        .foregroundStyle(.white)
-                    
-                    Text("Adoro um filme bom!")
-                        .font(.subheadline)
-                        .fontWeight(.light)
-                        .foregroundStyle(.white)
-                }.padding(.top, 260)
-                
                 VStack{
-                    
-                    HStack{
+                    ZStack {
+                        Image("tiraPerfil")
+                           Circle()
+                               .stroke(Color(red: 29/255, green: 241/255, blue: 145/255), lineWidth: 8)
+                               .frame(width: 150, height: 150)
+
+                           Image("perfil")
+                               .resizable()
+                               .scaledToFill()
+                               .frame(width: 143, height: 143)
+                               .clipShape(Circle())
+                       }
+            
+                    VStack(alignment: .center, spacing: 15){
                         
-                    }
+                        Text("RATATA")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                        
+                        Text("Adoro um filme bom!")
+                            .font(.subheadline)
+                            .fontWeight(.light)
+                            .foregroundStyle(.white)
+                    }.padding(.bottom, 35)
+                    
+
+                    VStack(alignment: .leading){
+                        
+                       
+                        HStack{
+                            Text("Top Filés")
+                                .padding(.horizontal,17)
+                                .padding(.vertical, 5)
+                                .font(.subheadline)
+                                .background(Color(red: 29/255, green: 241/255, blue: 145/255))
+                                .cornerRadius(14)
+                            Spacer()
+                        }
+                        HStack{
+                            
+                        }
+                    }.padding(.horizontal, 20)
+                    Spacer()
+                        
                 }
-            }
-            
-            
-            
+                
+                }
+                        
             
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(red: 0.0, green: 15/255, blue: 58/255))
