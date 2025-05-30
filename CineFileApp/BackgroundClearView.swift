276c315efa84ca40ce9200dbd9f31fe82b65176a
DANI <romao@alunos.utfpr.edu.br>
2025-05-30 11:00:37 -0300

    UPDATE login

---
+++ CineFileApp/BackgroundClearView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundClearView.swift
+//  CineFileApp
+//
+//  Created by Thayssa Romão on 30/05/25.
+//
+
+import SwiftUI
+
+struct BackgroundClearView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
