cceb22a202587f3f1a0a1669e97e48b47a67c946
ana23lua <analluy@icloud.com>
2025-06-02 08:22:58 -0300

    feat card e scroll view

--- CineFileApp/homeView.swift
+++ CineFileApp/homeView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct homeView: View {
     
-    @State var isLiked = false
+    @State var isLiked: Bool = false
+    @State var isViewed: Bool = false
+    
+    let dataModel = DataModel()
     
     var body: some View {
         VStack (alignment: .center){
@@ -23,110 +26,89 @@ struct homeView: View {
                     .padding(.top, 60)
                 
             }
-                Text("Top filés")
-                    .font(.title)
+            Text("Top filés")
+                .font(.title)
+                .bold()
+            
+            Divider()
+                .frame(width: 200, height: 1)
+                .background(Color.gray)
+                .padding(.bottom, 20)
+            
+            
+            
+            ZStack(alignment: .top){
+                
+                Image("capamonica")
+                    .scaleEffect(0.6)
+                    .frame(width: 360, height: 200)
+                    .overlay(
+                        LinearGradient(
+                            gradient: Gradient(stops: [
+                                .init(color: .blue.opacity(0.1), location: 0.5),
+                                .init(color: .black.opacity(0.8), location: 1.0)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                            
+                        ).blendMode(.multiply))
+                    .cornerRadius(25)
+                
+                Rectangle() // rosa
+                    .foregroundColor(.pink)
+                    .frame(width: 50, height: 30)
+                    .cornerRadius(15)
+                    .position(x:340,y:170)
+                    .overlay(
+                        Button(action: {
+                            isLiked.toggle()
+                        }) {
+                            Image(systemName: isLiked ? "heart.fill" : "heart")
+                                .foregroundStyle(.white)
+                                .position(x: 340, y: 170)
+                        }
+                    )
+                Rectangle()//verde
+                    .frame(width: 110, height: 30)
+                    .cornerRadius(15)
+                    .position(x:80, y:1)
+                    .foregroundStyle(.green)
+                
+                Text("Só os files")
+                    .position(x:80, y:1)
+                    .foregroundStyle(.black)
                     .bold()
                 
-                Divider()
-                    .frame(width: 200, height: 1)
-                    .background(Color.gray)
-                    .padding(.bottom, 20)
-                
-                
-                
-                ZStack(alignment: .top){
-                    
-                    Image("capamonica")
-                        .scaleEffect(0.6)
-                        .frame(width: 360, height: 200)
-                        .overlay(
-                            LinearGradient(
-                                gradient: Gradient(stops: [
-                                    .init(color: .blue.opacity(0.1), location: 0.5),
-                                    .init(color: .black.opacity(0.8), location: 1.0)
-                                ]),
-                                startPoint: .top,
-                                endPoint: .bottom
-                                
-                            ).blendMode(.multiply))
-                        .cornerRadius(25)
-                    
-                    Rectangle() // rosa
-                        .foregroundColor(.pink)
-                        .frame(width: 50, height: 30)
-                        .cornerRadius(15)
-                        .position(x:340,y:170)
-                        .overlay(
-                            Button(action: {
-                                isLiked.toggle()
-                            }) {
-                                Image(systemName: isLiked ? "heart.fill" : "heart")
-                                    .foregroundStyle(.white)
-                                    .position(x: 340, y: 170)
-                            }
-                        )
-                    Rectangle()//verde
-                        .frame(width: 110, height: 30)
-                        .cornerRadius(15)
-                        .position(x:80, y:1)
-                        .foregroundStyle(.green)
-                    
-                    Text("Só os files")
-                        .position(x:80, y:1)
-                        .foregroundStyle(.black)
-                        .bold()
-                    
-                }
-                
-                Text("Popular entre os academers")
-                    .font(.title)
-                    .bold()
-                
-                Divider()
-                    .frame(height: 1)
-                    .background(Color.gray)
-                    .padding(.bottom, 10)
-                    .padding(.bottom, 15)
-                
+            }
+            
+            Text("Popular entre os academers")
+                .font(.title)
+                .bold()
+            
+            Divider()
+                .frame(height: 1)
+                .background(Color.gray)
+                .padding(.bottom, 10)
+                .padding(.bottom, 15)
+            
                 ScrollView(.horizontal) {
                     HStack(alignment: .center){
-                        Image("filme1")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
-                        Image("filme2")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
-                        Image("filme3")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
-                        Image("filme4")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
-                        Image("filme5")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
-                        Image("filme6")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
-                        Image("filme7")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
-                        Image("filme8")
-                            .resizable()
-                            .frame(width: 120, height: 180)
-                            .cornerRadius(15)
+                        ForEach(dataModel.filmList){ film in
+                            //navigation link é o botao que navega
+                            NavigationLink {
+                                //parte de cima é a view de destino // a outra também tem que ter navigationbar
+                                filmDetailView(film: film)
+                            } label: {
+                                //aqui em baixo é como a navigation link é
+                                filmCapaView(film: film)
+                            }
+                        }
                     }
                     .padding(.horizontal, 15)
                     Spacer()
-            }
-            Spacer()
+                }
+                Spacer()
+            
         }
         Spacer()
         }
---
+++ CineFileApp/model/FilmCardView.swift
@@ -0,0 +1,94 @@
+import SwiftUI
+
+struct FilmCardView: View {
+    let film: Film
+    @State private var isLiked = false
+    @State private var isViewed = false
+    
+    var body: some View {
+        VStack{
+            VStack(alignment: .center, spacing: 15) {
+                
+                ZStack{
+                    Image(film.foto)
+                        .resizable()
+                        .scaledToFill()
+                        .clipped()
+                    
+                    LinearGradient(
+                        gradient: Gradient(stops: [
+                            .init(color: .clear, location: 0.0),
+                            .init(color: .white, location: 1.0)
+                        ]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                }
+                
+                HStack(alignment: .top){
+                    Image(film.image)
+                        .resizable()
+                        .frame(width: 120, height: 180)
+                        .cornerRadius(15)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(film.title)
+                            .font(.title3)
+                            .bold()
+                            .fixedSize(horizontal: false, vertical: true)
+                        
+                        Text(film.director)
+                            .foregroundStyle(.secondary)
+                        
+                        Button(action: {
+                            isLiked.toggle()
+                        }) {
+                            Image(systemName: isLiked ? "heart.fill" : "heart")
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(Color.pink)
+                                .clipShape(Circle())
+                        }
+                    }
+                    Spacer()
+                        
+                    
+                }
+                .padding(.horizontal, 20)
+                
+                
+                
+                VStack(alignment:.center){
+                    Text(film.description)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 20)
+                    
+                    Rectangle()
+                        .foregroundColor(.blue)
+                                                   .frame(width: 50, height: 30)
+                                                   .cornerRadius(15)
+                                                   .overlay(
+                                                       Button(action: {
+                                                           isViewed.toggle()
+                                                       }) {
+                                                           Image(systemName: isViewed ? "eye" : "eye.fill")
+                                                               .foregroundStyle(.white)
+                                                       }
+                                                   )
+                                                   .padding(.vertical, 20)
+                }
+                
+            }
+            .background(Color.white)
+            
+            
+        }
+        .scaledToFit()
+        .cornerRadius(20)
+        .padding(.vertical,10)
+    }
+}
+#Preview {
+    FilmCardView(film: Film(image:"filme1", title: "Tudo em Todo o Lugar ao Mesmo Tempo", director: "Daniel Kwan, Daniel Scheinert", description: "Uma imigrante chinesa de meia idade se envolve em uma aventura louca, onde só ela pode salvar o mundo explorando outros universos que se conectam com as vidas que ela poderia ter levado", foto: "filme1capa"))
+}
